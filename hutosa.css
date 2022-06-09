f9399a7a93b10192475bf65eefd9580101063689
masa-proj <98656010+masa-proj@users.noreply.github.com>
2022-06-09 12:38:48 +0900

    Create hutosa.css

---
+++ hutosa.css
@@ -0,0 +1,25 @@
+//テキストを含む一般的なモーダル
+$(".info").modaal({
+    overlay_close:true,//モーダル背景クリック時に閉じるか
+    before_open:function(){// モーダルが開く前に行う動作
+      $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+    },
+    after_close:function(){// モーダルが閉じた後に行う動作
+      $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+    }
+  });
+
+    
+  //iframeのモーダル
+  $(".iframe-open").modaal({
+      type:'iframe',
+      width: 2000,//iframe横幅
+      height:2000,//iframe高さ
+      overlay_close:true,//モーダル背景クリック時に閉じるか
+    before_open:function(){// モーダルが開く前に行う動作
+      $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+    },
+    after_close:function(){// モーダルが閉じた後に行う動作
+      $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+    }
+  });
